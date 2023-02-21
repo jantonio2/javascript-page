9d8e830fae3c0f385acdd83844badd1f69650467
jantonio2 <jesusrochavedia@gmail.com>
2023-02-20 23:52:26 -0400

    Create user model

---
+++ src/users/models/user.js
@@ -0,0 +1,17 @@
+export class User {
+
+  /**
+   * 
+   * @param {Object<User>} userData 
+   */
+  constructor({ id, isActive, balance, avatar, firstName, lastName, gender }) {
+    this.id = id
+    this.isActive = isActive
+    this.balance = balance
+    this.avatar = avatar
+    this.first_name = firstName
+    this.last_name = lastName
+    this.gender = gender
+  }
+
+}
